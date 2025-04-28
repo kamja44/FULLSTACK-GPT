af87d17208ddd85c35dbc19ed79b5153863ad574
kamja44 <gudgh5007@gmail.com>
2025-04-28 09:19:21 +0900

    7.2 Data Flow

--- Home.py
+++ Home.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from langchain.prompts import PromptTemplate
+from datetime import datetime
+
+today = datetime.today().strftime("%H:%M:%S")
+
+st.title(today)
 
 st.write("Hello")
 a = [1,2,3,4]
@@ -10,3 +15,6 @@ a
 d
 
 st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
+
+value = st.slider('temperature', min_value=0.1, max_value=1.0)
+st.write(value)
