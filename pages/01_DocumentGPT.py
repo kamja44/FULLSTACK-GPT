c5c792b29dd77e94ecccbcb6be53a92c8d4fe968
kamja44 <gudgh5007@gmail.com>
2025-04-28 10:58:00 +0900

    7.4 Chat Messages

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -3,24 +3,28 @@ import streamlit as st
 
 st.title("DocumentGPT")
 
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-# chat_message를 human으로 설정했기 때문에 with 아래에 작성된 모든 것들은 human이 작성한 것이 된다.
-with st.chat_message("human"):
-    st.write("Hello")
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
 
-# 마찬가지로 모두 ai가 작성한게 된다.
-with st.chat_message("ai"):
-    st.write("how are you")
+for message in st.session_state["messages"]:
+    send_message(
+        message["message"],
+        message["role"],
+        save=False,
+    )
 
-st.chat_input('Send a message to the ai')
-
-# chatbot이 어떤 작업을 할 때 커뮤니케이션을 만들 수 있다.
-with st.status("Embedding file...", expanded=True) as status:
+message = st.chat_input("Send a message to the ai ")
+ 
+if message:
+    send_message(message, "human")
     time.sleep(2)
-    st.write("Getting the file")
-    time.sleep(2)
-    st.write("Embedding the file")
-    time.sleep(2)
-    st.write("Caching the file")
-    status.update(label="Error", state="error")
+    send_message(f"You said: {message}", "ai")
 
+    with st.sidebar:
+        st.write(st.session_state)
